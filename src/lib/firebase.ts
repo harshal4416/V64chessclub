9bc74f79ab806be09cfb0560fb2845d87f4b5001
Harshal Chaudhari <harshalchaudhari.14.hc@gmail.com>
2026-03-19 01:04:36 +0530

    firebase fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,11 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-
     admin.initializeApp({
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            // Handle the case where the private key might be escaped in the env var
-            privateKey: privateKey?.replace(/\\n/g, '\n'),
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, '\n'),
         }),
     });
 }
