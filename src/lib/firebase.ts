d145cbcb3c37371962b0471c1594bc2c0b0a8834
Harshal Chaudhari <harshalchaudhari.14.hc@gmail.com>
2026-03-19 21:46:28 +0530

    fix firebase private key issue

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,10 +3,9 @@ import * as admin from 'firebase-admin';
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            // Support both escaped \n and literal newlines
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')?.replace(/\n/g, '\n'),
+            projectId: process.env.FIREBASE_PROJECT_ID as string,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+            privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
         }),
     });
 }
